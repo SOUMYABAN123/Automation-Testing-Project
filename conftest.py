cbc7eebd117ad29f8586719861095813a66a8e7a
SOUMYABAN123 <101491490+SOUMYABAN123@users.noreply.github.com>
2025-05-28 16:05:37 +0200

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from playwright.sync_api import sync_playwright
+
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    return {**browser_context_args, "viewport": {"width": 1280, "height": 720}}
+
+@pytest.fixture(scope="session")
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        yield browser
+        browser.close()
